0782571d5157f335a96c33202a3931bc9ef5b8f8
shiva <shivagupta529@gmail.com>
2020-09-05 01:23:02 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './main/component/header/header.component';
+import { SearchComponent } from './main/component/header/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
